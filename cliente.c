b1b05d2a6ff6e8f7f953ecd4b7e053109bc3d14f
Fede <fede.bergagna@gmail.com>
2019-06-19 14:23:05 -0300

    Server con entendido y cliente

---
+++ cliente.c
@@ -0,0 +1,112 @@
+
+// Client side C/C++ program to demonstrate Socket programming 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+       #include <sys/types.h>
+       #include <sys/stat.h>
+       #include <fcntl.h>
+
+#define PORT 23423 
+
+void clear_msg(char msg[1024]){
+    for(int i = 0;i<1024;i++){
+        msg[i] = '\0';
+    }
+}
+   
+int main(int argc, char const *argv[]) 
+{ 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "entendido\n"; 
+    char buffer[1024] = {0}; 
+    char msg[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    }
+    int i=0;
+    char c;
+
+    //char * buff = malloc(1024);
+    //read(9,buff,1024);
+
+    //printf("%s\n",buff);
+    //free(buff);
+
+// 	write(sock , "entendido\n" , 10);
+//     sleep(1);
+//         write(sock , "#0854780*\n" ,10 );
+//     sleep(1);
+
+//     write(sock , "nokia\n" , 6);
+//     sleep(1);
+
+//     write(sock , "cabeza de calabaza\n" , 19);
+//         sleep(1);
+
+//     write(sock , "easter_egg\n" , 11);
+//     sleep(1);
+
+//     write(sock , ".runme\n" , 7);
+//     sleep(1);
+
+//     write(sock , "indeterminado\n" , 14);
+//     sleep(1);
+
+//     write(sock , "this is awesome\n" , 16);
+//     sleep(1);
+//     write(sock , "cachiporra\n" , 11);
+// sleep(1);
+//     write(sock , "gdb rules\n" , 10);
+// sleep(1);
+//     write(sock , "/lib/x86_64-linux-gnu/ld-2.19.so\n" , 33);
+
+
+
+
+
+    while(1){
+        i=0;
+
+        while((c = getchar()) != '\n'){
+            msg[i] = c;
+            i++;
+        }
+        msg[i] = '\n';
+        write(sock , msg , strlen(msg));
+	
+ 	//printf("%s",buff);
+        clear_msg(msg);
+
+    }
+
+   
+
+
+    return 0; 
+} 
---
+++ ourServer.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+#define PORT 23423 
+#define SIZE 1024
+#define SA struct sockaddr 
+  
+// Function designed for chat between client and server. 
+void challenge1(int sockfd); 
+
+void func(int sockfd) 
+{ 
+    char * buff = malloc(1024);
+    int n; 
+    // infinite loop for chat 
+    for(int i =0;i<12;i++){ 
+        //clear buffer
+        bzero(buff, SIZE); 
+  
+        // read the message from client and copy it in buffer 
+        read(sockfd, buff, sizeof(buff)); 
+        // print buffer which contains the client contents 
+        printf("From client: %s\t To client : ", buff); 
+        bzero(buff, SIZE); 
+        n = 0; 
+        // copy server message in the buffer 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+  
+        // and send that buffer to client 
+        write(sockfd, buff, sizeof(buff)); 
+  
+        // if msg contains "Exit" then server exit and chat ended. 
+        if (strncmp("exit", buff, 4) == 0) { 
+            printf("Server Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+  
+// Gracias por tanto Geeks for Geeks
+int main() 
+{ 
+    int sockfd, connfd, len; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and verification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+        printf("setsockopt(SO_REUSEADDR) failed");
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+  
+    // Binding newly created socket to given IP and verification 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+  
+    // Now server is ready to listen and verification 
+    if ((listen(sockfd, 5)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    len = sizeof(cli); 
+  
+    // Accept the data packet from client and verification 
+    connfd = accept(sockfd, (SA*)&cli, &len); 
+    if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
+    
+        //printf("server acccept the client...\n"); 
+  
+    // Function for chatting between client and server 
+    //func(connfd); 
+    challenge1(connfd);
+  
+    // After chatting close the socket 
+    close(sockfd); 
+} 
+
+void challenge1(int sockfd) 
+{ 
+    char * buff = malloc(1024);
+    int n; 
+    // infinite loop for chat 
+        while(1){ 
+        //clear buffer
+            printf("Bienvenidos al Himalaya, pone \"entendido\\n\" porque se pudre\n" ); 
+        bzero(buff, SIZE); 
+        
+        // read the message from client and copy it in buffer 
+        int last = recv(sockfd, buff, SIZE,0);
+     
+        printf("%s\n", buff);
+
+        // print buffer which contains the client contents 
+        
+        if(strcmp(buff,"entendido\n") == 0){
+            printf("Bien, sabes escribir\n");
+            break;
+        }
+        else{
+            printf("Que se siente que tus papas sean primos?\n");
+            sleep(1);
+            system("clear");
+        }
+
+
+    } 
+} 
